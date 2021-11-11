40b347360a56ff8849efff1000279974d81bb351
Pavan699 <pavannakate03@gmail.com>
2021-11-11 18:23:12 +0530

    [Pavan] Add: Print sorted tuple list with 2nd tuple item

---
+++ Lists/SortedTupleList.py
@@ -0,0 +1,30 @@
+
+'''
+@Author: Pavan Nakate
+@Date: 2021-11-11 06:05
+@Last Modified by: Pavan Nakate
+@Last Modified time: None
+@Title : SortedTupleList : Print the list in sorted manner with respect to 2nd tuple element  
+'''
+def sum_of_all():
+    """
+    Description:
+        This Function print the list in sorted manner with respect to 2nd tuple element
+    Parameter:
+        None
+    Return:
+        None
+    """
+    try:
+        #list of tuples
+        sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+        print("Tuples List : ",sample_list)
+        def last(n):
+            return n[-1]
+        print("Sorted list : ",sorted(sample_list,key=last))
+        
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    sum_of_all()
